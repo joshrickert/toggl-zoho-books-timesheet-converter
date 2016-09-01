59577d2d69a2ace0991677e9701c57412421ffe7
Josh Rickert <rickertjosh@gmail.com>
2016-09-01 13:38:26 -0700

    Working initial version

---
+++ README.md
@@ -0,0 +1,14 @@
+Convert Toggl timesheet CSV to Zoho Books format
+
+# Usage
+```
+node index.js Toggl_time_entries_2016-08-01_to_2016-08-31\ 11.59.43\ a.csv > out.csv
+```
+
+# Preparing CSV
+- "Detailed" view
+- Rounding on
+- Download as CSV
+
+# Importing to Zoho Books
+Use suggested field mappings specified in `field-mappings.png`.
---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const moment = require('moment');
+const csv = require('fast-csv');
+
+const inputFile = process.argv[2];
+
+function reformatTime(val) {
+  return moment(val, 'HH:mm:ss').format('HH:mm');
+}
+
+csv
+  .fromPath(inputFile, { headers: true })
+  .transform(row => {
+    row.Billable = 'Yes' === row.Billable ? 'Billable' : 'Non Billable';
+    row['Start time'] = reformatTime(row['Start time']);
+    row['End time'] = reformatTime(row['End time']);
+    row.Duration = reformatTime(row.Duration);
+    return row;
+  })
+  .pipe(csv.createWriteStream({headers: true}))
+  .pipe(process.stdout);
